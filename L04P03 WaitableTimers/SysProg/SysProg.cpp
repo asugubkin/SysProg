ebc7687af819463cc4e764eba9fa50a1535dcb61
dl <dl@bugtraq.ru>
2020-03-31 19:43:19 +0300

    (!) initial commit

---
+++ L04P03 WaitableTimers/SysProg/SysProg.cpp
@@ -0,0 +1,52 @@
+// SysProg.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "SysProg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+void start()
+{
+    _getch();
+}
+
+// The one and only application object
+
+CWinApp theApp;
+
+using namespace std;
+
+int main()
+{
+    int nRetCode = 0;
+
+    HMODULE hModule = ::GetModuleHandle(nullptr);
+            
+    if (hModule != nullptr)
+    {
+        // initialize MFC and print and error on failure
+        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+        {
+            // TODO: code your application's behavior here.
+            wprintf(L"Fatal Error: MFC initialization failed\n");
+            nRetCode = 1;
+        }
+        else
+        {
+            start();
+        }
+    }
+    else
+    {
+        // TODO: change error code to suit your needs
+        wprintf(L"Fatal Error: GetModuleHandle failed\n");
+        nRetCode = 1;
+    }
+
+    return nRetCode;
+}
